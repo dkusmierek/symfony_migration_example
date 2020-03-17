74347469a20c228be9abd4e10a0dea409ce5a24d
Dawid Kuśmierek <dawid@Dawids-MacBook-Pro.local>
2020-03-17 17:10:15 +0100

    Added web-profiler.

--- legacy/web/index.php
+++ legacy/web/index.php
@@ -3,7 +3,7 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', $_SERVER['APP_ENV'], false);
 $sfContext = sfContext::createInstance($configuration);
 ob_start();
 $sfContext->dispatch();
